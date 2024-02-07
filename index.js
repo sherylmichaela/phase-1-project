1f42c105c5d12cf2e7806609ab328c0f3d09d957
Sheryl Chee <sherylmichaela@gmail.com>
2024-02-07 22:25:39 +1100

    Added tooltip to Accessibility text in table header

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function initialize() {
   let greenBtn = document.querySelector("#greenBtn");
   let dropDown = document.querySelector("#dropdown");
 
+  // Tooltip
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
   dropDown.addEventListener("change", handleSelectChange);
   function handleSelectChange(event) {
     currentValue = event.target.value;
