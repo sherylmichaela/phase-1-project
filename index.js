29587178bf7fc20355c09e471651c86a626262f2
Sheryl Chee <sherylmichaela@gmail.com>
2024-02-07 22:31:49 +1100

    Added event listener to green button. Used fetch to get activity data from Bored API

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ function initialize() {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
+  // Event listener to access selected number inside dropdown menu
   dropDown.addEventListener("change", handleSelectChange);
   function handleSelectChange(event) {
     currentValue = event.target.value;
   }
+
+  greenBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let url = `http://www.boredapi.com/api/activity?participants=${currentValue}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {})
+      .catch((error) => {
+        console.log(`Oops, there seems to be an error. Pls try again.`);
+      });
+  });
 }
