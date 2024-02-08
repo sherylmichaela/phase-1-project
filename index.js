595e3240e9efecc01e56e947b997ff1785284a30
Sheryl Chee <sherylmichaela@gmail.com>
2024-02-08 22:10:39 +1100

    Created an array to store all keys generated

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function initialize() {
     currentValue = event.target.value;
   }
 
+  let keyArray = [];
+
   greenBtn.addEventListener("click", (event) => {
     event.preventDefault();
     let url = `http://www.boredapi.com/api/activity?participants=${currentValue}`;
@@ -23,60 +25,66 @@ function initialize() {
         return response.json();
       })
       .then((data) => {
-        let table = document.querySelector("#tbody");
-        let row = document.createElement("tr");
-        table.appendChild(row);
+        function addRow() {
+          let table = document.querySelector("#tbody");
+          let row = document.createElement("tr");
+          table.appendChild(row);
 
-        // Save button
-        saveBtn = document.createElement("button");
-        saveBtn.type = "button";
-        saveBtn.className = "btn btn-primary save-btn";
-        saveBtn.innerHTML = "Save";
+          // Save button
+          saveBtn = document.createElement("button");
+          saveBtn.type = "button";
+          saveBtn.className = "btn btn-primary save-btn";
+          saveBtn.innerHTML = "Save";
 
-        // Posting data to json server using the save button
-        saveBtn.addEventListener("click", () => {
-          return fetch("http://localhost:3000/data", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-            },
-            body: JSON.stringify({
-              activity: data.activity,
-              type: data.type,
-              accessibility: data.accessibility,
-            }),
-          }).then((response) => response.json());
-        });
+          // Posting data to json server using the save button
+          saveBtn.addEventListener("click", () => {
+            return fetch("http://localhost:3000/data", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
+              body: JSON.stringify({
+                activity: data.activity,
+                type: data.type,
+                accessibility: data.accessibility,
+              }),
+            }).then((response) => response.json());
+          });
 
-        // Delete button
-        deleteBtn = document.createElement("button");
-        deleteBtn.type = "button";
-        deleteBtn.className = "btn btn-danger delete-btn";
-        deleteBtn.innerHTML = "Delete";
+          // Delete button
+          deleteBtn = document.createElement("button");
+          deleteBtn.type = "button";
+          deleteBtn.className = "btn btn-danger delete-btn";
+          deleteBtn.innerHTML = "Delete";
 
-        deleteBtn.addEventListener("click", deleteRow);
+          deleteBtn.addEventListener("click", deleteRow);
 
-        // Create columns
-        let c1 = document.createElement("td");
-        let c2 = document.createElement("td");
-        let c3 = document.createElement("td");
-        let c4 = document.createElement("td");
-        let c5 = document.createElement("td");
+          // Create columns
+          let c1 = document.createElement("td");
+          let c2 = document.createElement("td");
+          let c3 = document.createElement("td");
+          let c4 = document.createElement("td");
+          let c5 = document.createElement("td");
 
-        // Activity data input into table
-        c1.innerHTML = data.activity;
-        c2.innerHTML = data.type.charAt(0).toUpperCase() + data.type.slice(1);
-        c3.innerHTML = data.accessibility;
-        c4.appendChild(saveBtn);
-        c5.appendChild(deleteBtn);
+          // Activity data input into table
+          c1.innerHTML = data.activity;
+          c2.innerHTML = data.type.charAt(0).toUpperCase() + data.type.slice(1);
+          c3.innerHTML = data.accessibility;
+          c4.appendChild(saveBtn);
+          c5.appendChild(deleteBtn);
 
-        // Adding td element with activity info to tr element
-        row.appendChild(c1);
-        row.appendChild(c2);
-        row.appendChild(c3);
-        row.appendChild(c4);
-        row.appendChild(c5);
+          // Adding td element with activity info to tr element
+          row.appendChild(c1);
+          row.appendChild(c2);
+          row.appendChild(c3);
+          row.appendChild(c4);
+          row.appendChild(c5);
+        }
+
+        // Appending/Pushing newly generated key into keyArray which stores the unique keys from activities already requested
+        keyArray.push(data.key);
+        console.log(keyArray);
       })
       .catch((error) => {
         console.log(`Oops, there seems to be an error. Pls try again.`);
